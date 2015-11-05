a11b00dad6a3f8c9ca0e4c24122deaa94d68614f
Will Schenk <wschenk@gmail.com>
2015-11-05 12:47:45 -0500

    Updated migration and ruby version for deployment

--- db/migrate/20151027213338_create_instagram_interactions.rb
+++ db/migrate/20151027213338_create_instagram_interactions.rb
@@ -1,7 +1,7 @@
 class CreateInstagramInteractions < ActiveRecord::Migration
   def change
     create_table :instagram_interactions do |t|
-      t.references :instagram_media, index: true, foreign_key: true
+      t.references :instagram_media, index: true
       t.integer :instagram_user_id
       t.string :comment
       t.boolean :is_like
@@ -11,6 +11,6 @@ class CreateInstagramInteractions < ActiveRecord::Migration
     add_index :instagram_interactions, :instagram_user_id
     add_index :instagram_interactions, :is_like
     # This is for pgsql.  Remove foreign_key: true above and replace with this
-    # add_foreign_key :instagram_interactions, :instagram_media, column: :instagram_media_id
+    add_foreign_key :instagram_interactions, :instagram_media, column: :instagram_media_id
   end
 end
